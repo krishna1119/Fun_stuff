d6dcaae3e19c784454c3dca0ab907fa0a1bb5522
Krishna Srinivas <krishnasrinivas2002@gmail.com>
2023-10-08 06:03:07 +0530

    first commit

---
+++ randomleetcode.py
@@ -0,0 +1,33 @@
+import random
+List = [
+    "ARRAY/STRING",
+    "TWO POINTERS",
+    "SLIDING WINDOW",
+    "MATRIX",
+    "HASH MAP",
+    "INTERVALS",
+    "STACK, QUEUE",
+    "LINKED LIST",
+    "BINARY TREE",
+    "BFS",
+    "DFS",
+    "BST",
+    "GRAPHS",
+    "TRIE",
+    "BACKTRACKING",
+    "DIVIDE AND CONQUER",
+    "KADANES ALGO",
+    "BINARY SEARCH",
+    "HEAP",
+    "BIT MAIOULATION",
+    "MATHS"
+    "DYNAMIC PROG",
+    "MULTI-DIMENSIONAL DP",
+    "SORT AND SEARCH",
+    "GREEDY APPROACH",
+    "UNIFORM SORT"
+]
+
+random_integer = random.randint(0,len(List))
+print("Your leetcode topic for the day is...................")
+print(List[random_integer])
